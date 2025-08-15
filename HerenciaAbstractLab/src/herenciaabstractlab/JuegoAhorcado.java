1a035e56a1d755cf97a1235d3fd1dacc8ee07260
ftMadrid <fernandomadridcv04@gmail.com>
2025-08-15 08:55:09 -0600

    Testing

---
+++ HerenciaAbstractLab/src/herenciaabstractlab/JuegoAhorcado.java
@@ -0,0 +1,9 @@
+package herenciaabstractlab;
+
+public abstract interface JuegoAhorcado {
+    
+    public abstract void inicializarPalabraSecreta();
+    
+    public abstract void jugar();
+    
+}
---
+++ HerenciaAbstractLab/src/herenciaabstractlab/JuegoAhorcadoBase.java
@@ -0,0 +1,22 @@
+package herenciaabstractlab;
+
+import java.util.ArrayList;
+
+public abstract class JuegoAhorcadoBase implements JuegoAhorcado{
+    
+    protected String palabraSecreta;
+    protected String palabraActual;
+    protected int intentos;
+    protected int limiteIntentos = 6;
+    protected ArrayList<Character> letraUsadas = new ArrayList<>();
+    protected ArrayList<String> figuraAhorcado = new ArrayList<>();
+    
+    public abstract char actualizarPalabraActual(char letra);
+    
+    public abstract char verificarletra(char letra);
+    
+    public abstract boolean hasGanado();
+    
+    
+    
+}
